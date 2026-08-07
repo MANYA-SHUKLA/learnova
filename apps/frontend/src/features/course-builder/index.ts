a77640ff7e981cbb846b480c825ad3d971438acb
Manya Shukla <work.shuklamanya@gmail.com>
2026-08-07 22:01:27 +0530

    first commit

---
+++ apps/frontend/src/features/course-builder/index.ts
@@ -0,0 +1,10 @@
+/**
+ * Course Builder feature barrel export
+ */
+
+export * from './types';
+export * from './services/builder-api';
+export * from './hooks/use-builder-queries';
+export * from './store/builder-store';
+export * from './lib/labels';
+export * from './components';
